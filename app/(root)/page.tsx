f7ce6975ec0ae03cf21d3e8d457815312f3a06b1
jayeshkumargoyal <kumarjayesh012@gmail.com>
2025-08-06 09:50:50 +0400

    layout implement done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Jayesh'};
+  const loggedIn = { firstName: 'Jayesh', lastName: 'Kumar', email: 'kumarjayesh012@gmail.com'};
   return (
     <section className="home">
       <div className="home-content">
@@ -20,7 +21,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
             />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 500.50}]}
+        />
     </section>
   )
 }
